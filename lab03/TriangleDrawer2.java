b626cc9b2d70685a89d42721187754d328ca8176
Hojung Lee <sandyhojunglee@berkeley.edu>
2019-06-28 00:50:41 -0700

    lab03 all files take 2

---
+++ lab03/TriangleDrawer2.java
@@ -0,0 +1,13 @@
+public class TriangleDrawer {
+    public static void main(String[] args){
+        int row = 0;
+        int SIZE = 10;
+        for (int row = 0; row<SIZE; row += 1) {
+            for (int col = 0; col < row; col += 1) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+}
